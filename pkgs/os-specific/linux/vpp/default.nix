269dcfcbee4038f28b76cb08947334bc16bad80f
Asumu Takikawa <asumu@igalia.com>
2017-10-26 15:15:58 -0700

    Clean up deps

--- pkgs/os-specific/linux/vpp/default.nix
+++ pkgs/os-specific/linux/vpp/default.nix
@@ -1,11 +1,11 @@
-{ stdenv, fetchgit, git, curl, autoconf, automake, ccache,
-  libtool, apr, libconfuse, chrpath, pkgconfig,
-  nasm, numactl, python27, libffi, openssl, jdk,
-  yacc,
-  # use nix dpdk package instead of statically linking
-  dpdk,
-  autoreconfHook
-  }:
+{ stdenv, fetchgit, git, autoconf, automake,
+  libtool, pkgconfig, python27, libffi,
+  # FIXME: these are needed if python/java APIs are enabled
+  # jdk,
+  openssl,
+  yacc, autoreconfHook,
+  # use nix dpdk package (default build downloads a dpdk)
+  dpdk }:
 
 let version = "17.07"; in
 
@@ -30,6 +30,7 @@ stdenv.mkDerivation rec {
   # Needed to build DPDk plugin with --static to avoid linking issues with Nix's DPDK
   patches = [ ./static.patch ];
 
+  # The python and java APIs caused build problems so don't build them for now
   configureFlags = [ "--disable-papi" "--disable-japi" ];
 
   NIX_CFLAGS_COMPILE = "-march=corei7 -mtune=corei7-avx";
