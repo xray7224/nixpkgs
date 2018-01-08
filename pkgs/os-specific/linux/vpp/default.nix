056d0c21cb7b8430bc9ba342818e0a73d3c05bfa
Jessica Tallon <tsyesika@igalia.com>
2018-01-08 17:08:26 +0100

    VPP converted to use "build-release"
    This in my opinion is a less clean method but it will allow VPP to build
    it's own DPDK package with it's own patchset. Before it would only build
    statically which VPP would not previously load.
    
    This is currently working locally in my local Nix VM. Next is testing on
    the server with real hardware.

--- pkgs/os-specific/linux/vpp/default.nix
+++ pkgs/os-specific/linux/vpp/default.nix
@@ -1,13 +1,16 @@
-{ stdenv, fetchgit, git, autoconf, automake,
+{ stdenv, fetchgit, git, autoconf, automake, curl,
   libtool, pkgconfig, python, pythonPackages, libffi,
+  nasm, numactl, openjdk,
   # FIXME: these are needed if python/java APIs are enabled
   # jdk,
   openssl,
-  yacc, autoreconfHook, ensureNewerSourcesHook,
-  # use nix dpdk package (default build downloads a dpdk)
-  dpdk }:
+  yacc, autoreconfHook, ensureNewerSourcesHook }:
 
-let version = "17.07"; in
+let
+  version = "17.10";
+
+  deps = [ openssl ];
+in
 
 stdenv.mkDerivation rec {
   name = "vpp-${version}";
@@ -15,11 +18,11 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://gerrit.fd.io/r/vpp";
     rev = "refs/tags/v${version}";
-    sha256 = "0llvn9h5yizdjwdb0f4z7sw27c8qyqw9iqkwwmmvzqrmwcar6dng";
   };
 
+  # Create the version file manually.
   preBuild = ''
-    cat > vpp/app/version.h <<EOF
+    cat > src/vpp/app/version.h <<EOF
     #define VPP_BUILD_DATE "Thu Jan 01 00:00:01 UTC 1970"
     #define VPP_BUILD_USER "$(whoami)"
     #define VPP_BUILD_HOST "nix"
@@ -27,30 +30,69 @@ stdenv.mkDerivation rec {
     #define VPP_BUILD_VER "${version}"
     EOF
   '';
-
+  
   # git needed to create version.h
   nativeBuildInputs = [ autoconf automake libtool pkgconfig yacc openssl git
-                        autoreconfHook pythonPackages.setuptools
+                        curl pythonPackages.setuptools nasm numactl
                         # Needed to build Python API (zip 1980 issue)
                         (ensureNewerSourcesHook { year = "1980"; }) ];
-  buildInputs = [ python dpdk ];
+  buildInputs = [ python openjdk ];
 
-  sourceRoot = "vpp/src";
+  sourceRoot = "vpp";
 
-  # Needed to build API libraries
-  dontDisableStatic = true;
-
-  patches = [ # Needed to build DPDk plugin with --static to avoid linking issues with Nix's DPDK
-              ./static.patch ];
-
-  # The java APIs caused build problems so don't build them for now
-  configureFlags = [ "--disable-japi" ];
+  makeFlags = [ "PLATFORM=vpp TAG=vpp" ];
+  
+  patches = [];
 
   NIX_CFLAGS_COMPILE = "-march=corei7 -mtune=corei7-avx";
+  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  
+  buildFlags = [ "build-release" ];
 
-  # Fix up RPATH since TMPDIR seems to end up in it for some reason
+  # Doesn't seem like the best way... but there isn't a "make install", this works.
+  installPhase = ''
+    cd build-root/install-vpp-native
+
+    echo "Installing to $out"
+    mkdir $out
+
+    # Install VPP.
+    cp -r vpp/* $out/
+
+    # Install DPDK
+    cp -r dpdk/* $out/
+
+    # Erm, I think I need to do this.
+    ln -s ${openssl.out}/lib/libcrypto.so $out/lib/libcrypto.so.1.0.0
+  '';
+  postInstall = "cd ../..";
+
+  dontInstallCheck = true;
+
+  # We're not using the ./configure script directly so unfortunately we can't pass
+  # --prefix, this means we're having to patch everything after the fact - Ugh.
+  # Maybe there's a better way?
   preFixup=''
-    patchelf --set-rpath "$out/lib" "$out"/bin/vppapigen
+    patchelf --set-rpath "$out/lib64" "$out"/bin/elftool
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vppapigen
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vppctl
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_restart
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_api_test
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_json_test
+    patchelf --set-rpath "$out/lib64" "$out"/bin/svmtool
+    patchelf --set-rpath "$out/lib64" "$out"/bin/svmdbtool
+    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_get_metrics
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvatplugin.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibmemory.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvlib.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvapiclient.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libsvm.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvppcom.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libsvmdb.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibmemoryclient.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvnet.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibsocket.so.0.0.0"
   '';
 
   meta = with stdenv.lib; {
