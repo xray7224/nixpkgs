80cd4981cd2942ad297f4d5b0e0a57d0b1540d95
Jessica Tallon <tsyesika@igalia.com>
2018-01-10 13:09:14 +0100

    Update VPP package to 18.01 (pre-release)

--- pkgs/os-specific/linux/vpp/default.nix
+++ pkgs/os-specific/linux/vpp/default.nix
@@ -1,23 +1,21 @@
 { stdenv, fetchgit, git, autoconf, automake, curl,
   libtool, pkgconfig, python, pythonPackages, libffi,
-  nasm, numactl, openjdk,
+  nasm, numactl, openjdk, boost,
   # FIXME: these are needed if python/java APIs are enabled
   # jdk,
   openssl,
   yacc, autoreconfHook, ensureNewerSourcesHook }:
 
-let
-  version = "17.10";
-
-  deps = [ openssl ];
-in
+let version = "18.01"; in
 
 stdenv.mkDerivation rec {
   name = "vpp-${version}";
 
   src = fetchgit {
     url = "https://gerrit.fd.io/r/vpp";
-    rev = "refs/tags/v${version}";
+    # When released, change to use the tag.
+    #rev = "refs/tags/v${version}";
+    branchName= "stable/1801";
   };
 
   # Create the version file manually.
@@ -32,13 +30,20 @@ stdenv.mkDerivation rec {
   '';
   
   # git needed to create version.h
-  nativeBuildInputs = [ autoconf automake libtool pkgconfig yacc openssl git
-                        curl pythonPackages.setuptools nasm numactl
-                        # Needed to build Python API (zip 1980 issue)
-                        (ensureNewerSourcesHook { year = "1980"; }) ];
+  nativeBuildInputs = [
+    autoconf automake libtool pkgconfig yacc openssl git
+    curl pythonPackages.setuptools nasm numactl
+    boost
+    # Needed to build Python API (zip 1980 issue)
+    (ensureNewerSourcesHook { year = "1980"; })
+  ];
+  
   buildInputs = [ python openjdk ];
 
   sourceRoot = "vpp";
+
+  # Needed to build API libraries
+  dontDisableStatic = true;
 
   makeFlags = [ "PLATFORM=vpp TAG=vpp" ];
   
@@ -49,7 +54,6 @@ stdenv.mkDerivation rec {
   
   buildFlags = [ "build-release" ];
 
-  # Doesn't seem like the best way... but there isn't a "make install", this works.
   installPhase = ''
     cd build-root/install-vpp-native
 
@@ -61,28 +65,24 @@ stdenv.mkDerivation rec {
 
     # Install DPDK
     cp -r dpdk/* $out/
-
-    # Erm, I think I need to do this.
-    ln -s ${openssl.out}/lib/libcrypto.so $out/lib/libcrypto.so.1.0.0
+    
   '';
   postInstall = "cd ../..";
 
   dontInstallCheck = true;
 
-  # We're not using the ./configure script directly so unfortunately we can't pass
-  # --prefix, this means we're having to patch everything after the fact - Ugh.
-  # Maybe there's a better way?
+  # Fix up RPATH since TMPDIR seems to end up in it for some reason
   preFixup=''
-    patchelf --set-rpath "$out/lib64" "$out"/bin/elftool
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vppapigen
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vppctl
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_restart
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_api_test
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_json_test
-    patchelf --set-rpath "$out/lib64" "$out"/bin/svmtool
-    patchelf --set-rpath "$out/lib64" "$out"/bin/svmdbtool
-    patchelf --set-rpath "$out/lib64" "$out"/bin/vpp_get_metrics
+    patchelf --set-rpath "$out/lib" "$out"/bin/vpp
+    patchelf --set-rpath "$out/lib" "$out"/bin/vppapigen
+    patchelf --set-rpath "$out/lib" "$out"/bin/vppctl
+    patchelf --set-rpath "$out/lib" "$out"/bin/vpp_api_test
+    patchelf --set-rpath "$out/lib" "$out"/bin/vpp_json_test
+    patchelf --set-rpath "$out/lib" "$out"/bin/vpp_get_metrics
+    patchelf --set-rpath "$out/lib" "$out"/bin/vpp_restart
+    patchelf --set-rpath "$out/lib" "$out"/bin/svmtool
+    patchelf --set-rpath "$out/lib" "$out"/bin/svmdbtool
+    patchelf --set-rpath "$out/lib" "$out"/bin/elftool
     patchelf --set-rpath "$out/lib64" "$out/lib64/libvatplugin.so.0.0.0"
     patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibmemory.so.0.0.0"
     patchelf --set-rpath "$out/lib64" "$out/lib64/libvlib.so.0.0.0"
@@ -92,7 +92,7 @@ stdenv.mkDerivation rec {
     patchelf --set-rpath "$out/lib64" "$out/lib64/libsvmdb.so.0.0.0"
     patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibmemoryclient.so.0.0.0"
     patchelf --set-rpath "$out/lib64" "$out/lib64/libvnet.so.0.0.0"
-    patchelf --set-rpath "$out/lib64" "$out/lib64/libvlibsocket.so.0.0.0"
+    patchelf --set-rpath "$out/lib64" "$out/lib64/libvom.so.0.0.0"
   '';
 
   meta = with stdenv.lib; {
